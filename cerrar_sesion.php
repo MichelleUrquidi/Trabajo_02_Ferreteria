dfbbc3d555064f544528d97fde796144230a99f0
michelleurquidi <urquidimichelle11@gmail.com>
2025-11-24 16:16:38 +0100

    Update08:Eliminar los ficheros de la primera version

--- cerrar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Elimina todas las variables de sesión
-session_destroy(); // Destruye la sesión
-
-// Redirigir a la página de login con un mensaje de cierre
-header('Location: login.php');
-exit;
-?>
